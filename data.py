d994e73caf3d71800bf5169298037a68999ea877
panpan2 <p.panayiotou2@gmail.com>
2017-03-19 00:38:54 +0000

    Add helpful comments and tidy code

--- data.py
+++ data.py
@@ -6,18 +6,19 @@ from sklearn.metrics import accuracy_score
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import roc_curve, auc
 
+def plot_wrt_variable(data, variable):
+    pass
 
 sns.set_style("whitegrid")
 
+# Input data from csv
 data = pds.read_csv('No-show-Issue-Comma-300k-corrected.csv')
 
+# Setup DataSegment
 data.Gender = data.Gender.apply(lambda x: 1 if x == 'M' else 0)
 days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 data.DayOfTheWeek = data.DayOfTheWeek.apply(lambda x: days.index(x))
 
-total = 296500
-train = int(0.8 * total)
-labels_train = data.Status[:train]
 
 feature_list = [
 'Age',
@@ -36,19 +37,21 @@ feature_list = [
 'Sms_Reminder',
 'AwaitingTime'
 ]
+
+# Use 80% for training, 20% for testing
+total = 296400
+train = int(0.8 * total)
+
 features_train = data[feature_list].iloc[:train]
-print(features_train)
 features_test = data[feature_list].iloc[train:]
-
 labels_test = data.Status[train:]
-print(labels_test)
-
+labels_train = data.Status[:train]
 
 # Multinomial naive bayes classification
 clf =  MultinomialNB().fit(features_train, labels_train)
-print('Accuracy:', round(accuracy_score(labels_test, 
-                                        clf.predict(features_test)), 2) * 100, '%')
+print('Accuracy: ', round(accuracy_score(labels_test, clf.predict(features_test)), 3) * 100, '%')
 
+# AUC
 results = clf.predict_proba(features_test)
 print(results)
 fpr = dict()
@@ -56,8 +59,13 @@ tpr = dict()
 roc_auc = dict()
 fpr, tpr, _ = roc_curve(labels_test, [i[1] for i in results])
 roc_auc = auc(fpr, tpr)
-print(roc_auc)
+print('AUC: ', roc_auc)
 plt.plot(fpr, tpr, 'ro')
+
+# Plot how each variable independently affects the probability of showing up
+for variable in feature_list:
+    plot_wrt_variable(data, variable)
+
 plt.show()
 
 
